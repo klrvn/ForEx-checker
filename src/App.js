17019c0dc53b0db1bf4d23cfb6f8f2fb7c217a1f
klrvn <kevin.runbo@gmail.com>
2025-05-24 12:45:07 +0800

    changed wording

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Exchange Rate Checker</h1>
+      <p>This is a simple React app deployed on Netlify.</p>
     </div>
   );
 }
